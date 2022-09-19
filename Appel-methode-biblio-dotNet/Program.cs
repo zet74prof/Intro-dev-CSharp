ddb060830f53b24e26a40b453d7763bf8a00774d
sio <sio@SIO-0-00>
2022-09-19 22:05:06 +0200

    Ajoutez des fichiers projet.

---
+++ Appel-methode-biblio-dotNet/Program.cs
@@ -0,0 +1,27 @@
+﻿//Appel de la méthode WriteLine() de la classe statique Console
+Console.WriteLine("J\'ai appelé ici la méthode WriteLine() depuis la classe Console");
+
+//Certaines classes ne peuvent être utilisé directement comme la classe Console qu'on dit "static"
+//Il faut alors créer une instance de cette classe avec l'instruction new et stocker cette instance dans une variable
+
+//1-Création de l'instance de la classe Random.
+//Instance de classe = variable d'un type particulier définie dans la bibliothèque .Net = un objet
+Random unObjetDeLaClasseRandom = new Random();
+//2-appel de la méthode Next() de la classe Random à partir de l'objet créé
+//Cette méthode renvoie comme résultat un nombre entier aléatoire entre 1 et 6 qu'on récupère dans la variable chiffreAleatoire
+//(1,7) sont les 2 paramètres d'entrée attendus par la méthode Next()
+int chiffreAleatoire = unObjetDeLaClasseRandom.Next(1, 7);
+//3-Affichage du résultat
+Console.WriteLine("Mon chiffre aléatoire est: " + chiffreAleatoire);
+
+//Surcharge de méthode = la même méthode peut être appelé avec différents paramètres d'entrée
+//Exemple avec la méthode Next() de la classe Random
+chiffreAleatoire = unObjetDeLaClasseRandom.Next();
+Console.WriteLine("Avec Next(), mon chiffre aléatoire est: " + chiffreAleatoire + "(entre 0 et l'infini)");
+chiffreAleatoire = unObjetDeLaClasseRandom.Next(100);
+Console.WriteLine("Avec Next(100), mon chiffre aléatoire est: " + chiffreAleatoire + "(situé entre 0 et 99)");
+//Note: les paramètres d'une méthode sont séparés par une virgule
+chiffreAleatoire = unObjetDeLaClasseRandom.Next(1, 7);
+Console.WriteLine("Avec Next(1,7), mon chiffre aléatoire est: " + chiffreAleatoire + "(situé entre 1 et 6)\n");
+
+//Quand elles ont terminé leur travail, les méthodes peuvent retourner une valeur ou rien du tout
---
+++ Logique-booleene/Program.cs
@@ -0,0 +1,63 @@
+﻿MiseEnSituation();
+Exercice1();
+Exercice2();
+Exercice3();
+Exercice4();
+
+static void MiseEnSituation()
+{
+
+    // https://docs.microsoft.com/fr-fr/dotnet/csharp/language-reference/builtin-types/bool
+    bool variableVraiFaux;
+    variableVraiFaux = 5 < 2;
+    variableVraiFaux = 5 >= 2;
+    variableVraiFaux = (5 == 2);
+    variableVraiFaux = (5 != 2);
+    variableVraiFaux = (2 > 2);
+    // https://docs.microsoft.com/fr-fr/dotnet/csharp/programming-guide/strings/
+    // https://www.c-sharpcorner.com/article/string-in-C-Sharp/
+    variableVraiFaux = ("salut" == "Salut");
+    // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/floating-point-numeric-types
+    variableVraiFaux = (1.001F == 1.001D);
+}
+static void Exercice1()
+{
+    string variableChaine;
+    int variableEntier;
+    bool variableVraiFaux;
+    variableEntier = 1 + 4; // attendu 5
+
+}
+static void Exercice2()
+{
+    string resultat;
+    const string C1 = "Lycée";
+    string x, y;
+    x = "Gabriel";
+
+}
+
+static void Exercice3()
+{
+    float resultatF;
+    bool resultatB;
+    const int C1 = 2;
+    const int C2 = 10;
+    float varX, varY;
+    int varA, varB;
+    varX = 12.5F;
+
+}
+
+static void Exercice4()
+{
+    string variableChaine;
+    float variableFlottant;
+    int variableEntier;
+    decimal variableDecimal;
+    bool variableVraiFaux;
+    char variableCaractere;
+
+    variableChaine = "true";
+
+}
